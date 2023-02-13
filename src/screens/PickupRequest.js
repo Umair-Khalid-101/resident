09bade68858872aff8465812a47b4685a46dbc20
Umair-Khalid-101 <umairkhalid.github@gmail.com>
2023-02-13 19:43:59 +0500

    Notification and PickupReq changes

--- src/screens/PickupRequest.js
+++ src/screens/PickupRequest.js
@@ -1,7 +1,12 @@
 import React from "react";
+import { View, Text } from "react-native";
 
 function PickupRequest(props) {
-  return <View>Hello</View>;
+  return (
+    <View>
+      <Text>Hello</Text>
+    </View>
+  );
 }
 
 export default PickupRequest;
